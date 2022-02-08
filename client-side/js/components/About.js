74ad26589943f83c51aa26fc211df7f35742adfe
landoncolesims <landoncolesims@gmail.com>
2022-02-08 13:47:04 -0500

    basic about page

--- client-side/js/components/About.js
+++ client-side/js/components/About.js
@@ -1,7 +1,59 @@
 import OnTheGo from '../../assets/images/OnTheGo.jpg';
 
 export default function About(){
-    return` 
-    <p>hello</p>
-    `;
+    return`
+    <div class="page__container">
+    <div class="aboutDisplay">
+    <div class = "bio">
+        <h2>Lyzz Hans</h2>
+        <img class="headshot" src="${OnTheGo}" alt="headshot-Lyzz">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <h4>Contact</h4>
+        <li>Personal Site: </li>
+        <li>Email: </li>
+    </div>
+    <div class = "bio">
+        <h2>Landon Sims</h2>
+        <img class="headshot" src="${OnTheGo}" alt="headshot-Landon">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <h4>Contact</h4>
+        <li>Personal Site: </li>
+        <li>Email: </li>
+    </div>
+    <div class = "bio">
+        <h2>Cameron Stephens</h2>
+        <img class="headshot" src="${OnTheGo}" alt="headshot-Cameron">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi libero nibh, cursus at eleifend a, sodales sit amet magna. Duis convallis orci venenatis nisl iaculis elementum. Nunc tempor nibh elit, id volutpat mi tempus tempor.</p>
+        <h4>Contact</h4>
+        <li>Personal Site: </li>
+        <li>Email: </li>
+    </div>
+    <div class = "bio">
+        <h2>Martel Barkley</h2>
+        <img class="headshot" src="${OnTheGo}" alt="headshot-Martel">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <h4>Contact</h4>
+        <li>Personal Site: </li>
+        <li>Email: </li>
+    </div>
+    <div class = "bio">
+        <h2>Trey Browder</h2>
+        <img class="headshot" src="${OnTheGo}" alt="headshot-Trey">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <h4>Contact</h4>
+        <li>Personal Site: </li>
+        <li>Email: </li>
+    </div>
+    <div class = "bio">
+    <h2>Dia Scruggs</h2>
+    <img class="headshot" src="${OnTheGo}" alt="headshot-Dia">
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <h4>Contact</h4>
+    <li>Personal Site: </li>
+    <li>Email: </li>
+</div>
+  
+</div>
+    </div>
+      `;
 }
