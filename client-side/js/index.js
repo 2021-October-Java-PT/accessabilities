db99382e1e6b3bb8d42b3e74caabcd5e49a7601c
CamS-World <cstephen13@mail.bw.edu>
2022-02-07 20:12:03 -0500

    added apiHelpers and made file for Login

--- client-side/js/index.js
+++ client-side/js/index.js
@@ -1,1 +1,9 @@
-console.log('Client Side is wired up!');
+import apiHelpers from "./apiHelpers";
+
+console.log("Client Side is wired up!");
+
+buildPage();
+
+function buildPage() {
+  console.log("PAGE BUILT!");
+}
