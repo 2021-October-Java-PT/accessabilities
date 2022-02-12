aaa29607df6bd9532b5baa5edb96f55d4007a5fe
DiaS793 <DiaScruggs793@outlook.com>
2022-02-12 15:44:59 -0500

    Added mapsResources to index and Maps to Components js. Still working on getting maps to show up :/

---
+++ client-side/js/Components/Maps.js
@@ -0,0 +1,12 @@
+export default function Maps(){
+    return `
+    <div class="activities>
+    <h1> MAPS!</h1>
+    <h2>Activities Map</h2>
+    
+    <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1hUuQOABuMohNFJmr6U10T9Rnfj1Su33j&ehbc=2E312F" width="640" height="480"></iframe>
+
+    <button class="returnToResources"> Return to Resources</>
+    </div>
+    `;
+}
--- client-side/js/index.js
+++ client-side/js/index.js
@@ -3,6 +3,7 @@ import Access from './components/Access';
 import Account from './components/Account';
 import Contact from './components/Contact';
 import Login from "./components/Login";
+import Maps from './Components/Maps';
 import Resources from './components/Resources';
 import Village from './components/Village';
 import apiHelpers from "./apiHelpers";
@@ -16,6 +17,7 @@ function renderPage() {
     contact();
     resources();
     village();
+    mapsResources();
 }
 
 
@@ -32,6 +34,25 @@ function login() {
     loginElem.addEventListener("click", () => {
         const page = document.querySelector(".Page");
         page.innerHTML = Login();
+    });
+}
+
+function mapsResources(){
+    const mapsElem = document.querySelector("#resources")
+    mapsElem.addEventListener('click',()=> {
+        PageContent.innerHTML = Resources();
+        clickMaps();
+    })
+}
+
+function clickMaps(){
+    PageContent.addEventListener("click", (event) => {
+        console.log("Is this thing on????")
+        PageContent.innerHTML = Maps();
+        if(event.target.classList.contains("returnToResources")){
+            PageContent.innerHTML= Resources();
+        }
+    
     });
 }
 
