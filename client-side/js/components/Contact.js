c351078ef1534e4f9cf135566a19a2a7d3f35d35
landoncolesims <landoncolesims@gmail.com>
2022-02-16 00:06:06 -0500

    bones of contact

--- client-side/js/components/Contact.js
+++ client-side/js/components/Contact.js
@@ -1,5 +1,18 @@
+import OnTheGo from '../../assets/images/OnTheGo.jpg';
+
 export default function Contact(){
+    //add responsiveness to mobile,faq animations/content,styling,replace placeholder pics
     return` 
-    <p>Contact the team for some help</p>
+    <h1>Contact</h1>
+    <div class="image-grid">
+        <img  class="image-grid-col-2 image-grid-row-2" src=${OnTheGo} ></img>
+        <img src=${OnTheGo} ></img>
+        <img src=${OnTheGo} ></img>
+        <img src=${OnTheGo} ></img>
+        <img src=${OnTheGo} ></img>
+    </div>
+    <p>Phone: 555-555-5555</p>
+    <p>Email: customerservice@accessaccessability.com</p>
+    <p>FAQ DropDown</p>
     `;
 }
