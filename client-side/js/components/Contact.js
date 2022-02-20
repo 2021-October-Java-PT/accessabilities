0081145e379cdae9c443eb641b15314f174caa56
landoncolesims <landoncolesims@gmail.com>
2022-02-20 14:36:41 -0500

    bones before animations and layout

--- client-side/js/components/Contact.js
+++ client-side/js/components/Contact.js
@@ -3,16 +3,16 @@ import OnTheGo from '../../assets/images/OnTheGo.jpg';
 export default function Contact(){
     //add responsiveness to mobile,faq animations/content,styling,replace placeholder pics
     return` 
+    <div class="wrapper">
     <h1>Contact</h1>
-    <div class="image-grid">
-        <img  class="image-grid-col-2 image-grid-row-2" src=${OnTheGo} ></img>
-        <img src=${OnTheGo} ></img>
-        <img src=${OnTheGo} ></img>
-        <img src=${OnTheGo} ></img>
-        <img src=${OnTheGo} ></img>
-    </div>
+    <img class="contact-image"src=${OnTheGo} ></img>
     <p>Phone: 555-555-5555</p>
     <p>Email: customerservice@accessaccessability.com</p>
     <p>FAQ DropDown</p>
+    <p>How can my business be added to your website?</p>
+    <p>How can I alert the community to my own experience at a location?</p>
+    <p>How do I leave a review?</p>
+    <p>What can I do if there is not a lot of content for my area?</p>
+    </div>
     `;
 }
