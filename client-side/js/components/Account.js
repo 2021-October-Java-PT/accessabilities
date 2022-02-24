d3990c587ab72d92b9ecd3039c41078894e25fd7
Lyzz Hans <90269867+LyzzHans@users.noreply.github.com>
2022-02-24 15:16:57 -0500

    deletelowercasec

--- client-side/js/components/Account.js
+++
@@ -1,5 +0,0 @@
-export default function Account(){
-    return` 
-    <p>Here is your account</p>
-    `;
-}
--- client-side/js/components/Contact.js
+++
@@ -1,5 +0,0 @@
-export default function Contact(){
-    return` 
-    <p>Contact the team for some help</p>
-    `;
-}
