b94965accad0162cb45122c01b2d0851d6a72d82
LyzzHans <lyzz.hans@gmail.com>
2022-02-24 15:02:29 -0500

    hopefix

--- client-side/js/components/About.js
+++ client-side/js/components/About.js
@@ -39,7 +39,7 @@ export default function About(){
     <div class = "bio">
         <h2>Trey Browder</h2>
        
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p>Lorem bacon dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         <h4>Contact</h4>
         <li>Personal Site: </li>
         <li>Email: </li>
