bbcee171eabee3810aed05b30a189840359e5264
DiaS793 <DiaScruggs793@outlook.com>
2022-02-25 12:13:54 -0500

    updated apps

--- client-side/js/Components/Village.js
+++ client-side/js/Components/Village.js
@@ -10,56 +10,64 @@ export default function Village() {
           </ul>
   
   
-        <div id="App_Grid">
-        <div class="apps">
-        <div class="devApp1">
+        
+        <div class="app_grid_wrapper">
+        <div class="app 1">
         <img id="app1"src='https://play-lh.googleusercontent.com/UiwJ_I6e0YOKtQRLUba3bhWMTOac58e6MDimJNUzzX0XPfycBFWu_ivp86PFGgBs1w=s180-rw' alt="Wheelmap App"/>
         <a href="https://play.google.com/store/apps/details?id=org.wheelmap.android.online&hl=en_US&gl=US"> Android: Wheelmap</a>
+        <br>
         <a href="https://apps.apple.com/au/app/wheelmap/id399239476"> Apple: Wheelmap</a>
         </div>
   
-        <div class="devApp2">
+        <div class="app 2">
         <img id="app1"src='https://play-lh.googleusercontent.com/1xTO2ugyjo0W9pmrm7w-BnAFmNYzlFILXioST9cwZ5RoYiBZifga2ArqCfXSXTwPxA=s180-rw' alt="Be My Eyes App"/>
         <a href="https://play.google.com/store/apps/details?id=com.bemyeyes.bemyeyes&hl=en_US&gl=US"> Android: Wheelmap</a>
+        <br>
         <a href="https://apps.apple.com/us/app/be-my-eyes/id905177575"> Apple: Wheelmap</a>
         </div>
   
-        <div class="devApp3">
+        <div class="app 3">
         <img id="app1"src='https://play-lh.googleusercontent.com/5oq216h9057MOMuxMbuHuss8KwQ05WI8qMxRYgHuxEUNlJ__2cxqcvihoLInWkSXS3E=s180-rw' alt="Legere Reader App"/>
         <a href="https://play.google.com/store/apps/details?id=voicedream.reader&hl=en_US&gl=US"> Android: Wheelmap</a>
         </div>
   
-        <div class="devApp4">
+        <div class="app 4">
         <img id="app1"src='https://play-lh.googleusercontent.com/RI2qcGUbzNJj_e_bYz46AoLPc8j3MNM9azW23FkJ6sQaVZHxrDlTeOdYvFvY3ZhiD659=s180-rw' alt="Miracle Modus App"/>
         <a href="https://play.google.com/store/apps/details?id=org.wheelmap.android.online&hl=en_US&gl=US"> Android: Miracle Modus</a>
+        <br>
         <a href="https://apps.apple.com/us/app/miracle-modus/id555904748"> Apple: Miracle Modus</a>
         </div>
   
-        <div class="devApp5">
+        <div class="app 5">
         <img id="app1"src='https://play-lh.googleusercontent.com/sofD2foSRv7E06VvHHam4FKkZv1M-6kR_sGQBL5BNXxxFn8fPllc2pR72tdpwlIzwQ=s180-rw' alt="Petralex Hearing Aid App"/>
         <a href="https://play.google.com/store/apps/details?id=com.it4you.petralex"> Android: Petralex Hearing Aid </a>
+        <br>
         <a href="https://apps.apple.com/us/app/petralex-hearing-aid-app/id816133779"> Apple: Petralex Hearing Aid </a>
         </div>
   
-        <div class="devApp6">
+        <div class="app 6">
         <img id="app1"src='https://play-lh.googleusercontent.com/CQT65mRy3t_fsVo8vJhRZ4iO4Uzps-3_SyRiLoXmYzK8Av-3_PZxGzS2xXEnKqNMwA=s180-rw' alt="Avaz AAC" />
         <a href="https://play.google.com/store/apps/details?id=com.avazapp.international.lite"> Android: Avaz AAC</a>
+        <br>
         <a href="https://apps.apple.com/us/app/avaz-aac/id909574843"> Apple: Avaz AAC</a>
         </div>
   
-        <div class="devApp7">
+        <div class="app 7">
         <img id="app1"src='https://play-lh.googleusercontent.com/TWJ7qI62stvVDDBo4c9yKy33Yujgjd3zigkI5RULPlm6ud5Nqq2dzb4gGWtzQwtchA=s180-rw' alt="Wheelmate App"/>
         <a href="https://play.google.com/store/apps/details?id=com.novasa.wheelmate"> Android: Wheelmate</a>
+        <br>
         <a href="https://apps.apple.com/us/app/wheelmate/id515277627"> Apple: Wheelmate</a>
         </div>
   
-        <div class="devApp8">
-        <img id="app1"src='https://apps.apple.com/us/app/choiceworks/id486210964' alt="Choiceworks App"/>
-        <a href="https://apps.apple.com/us/app/choiceworks/id486210964"> Apple: Choiceworks App</a>
+        <div class="app 8">
+        <img id="app1"src='https://apps.apple.com/us/app/choiceworks/id486210964' alt="Choiceworks App" />
+        <br>
+        <a href="https://apps.apple.com/us/app/choiceworks/id486210964"> Apple: Choiceworks App </a>
+        
         </div>
     
         </div>
-        </div>
+        
   
       `;
   }
