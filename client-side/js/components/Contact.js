60e84257e27b55895ee4bc6c228eb51b283327cb
landoncolesims <landoncolesims@gmail.com>
2022-02-27 12:10:44 -0500

    progress pushed, going to integrate contact forum to demenstrate more crud operations

--- client-side/js/components/Contact.js
+++ client-side/js/components/Contact.js
@@ -5,14 +5,49 @@ export default function Contact(){
     return` 
     <div class="wrapper">
     <h1>Contact</h1>
-    <img class="contact-image"src=${OnTheGo} ></img>
+    <h2>Maybe your questions already have answers!</h2>
+    <ul>
+        <li>
+            <div class = "dropdown">
+            How can I alert the community to my own experience at a location?
+                <div class="dropdown-content">
+                    <p class="desc">
+                        Fred the baker baking company is out here making donuts dont you know?
+                        We make all the donuts. We make so many donuts you wouldnt even believe it.
+                        Thats all you need to know isnt it? We make donuts. You buy them. See you here.
+                    </p>
+                </div>
+            </div>
+        </li>
+        <li>
+            <div class = "dropdown">
+            How do I leave a review?
+                <div class="dropdown-content">
+                    <p class="desc">
+                        Fred the baker baking company is out here making donuts dont you know?
+                        We make all the donuts. We make so many donuts you wouldnt even believe it.
+                        Thats all you need to know isnt it? We make donuts. You buy them. See you here.
+                    </p>
+                </div>
+            </div>
+        </li>
+        <li>
+            <div class = "dropdown">
+            What can I do if there is not a lot of content for my area?
+                <div class="dropdown-content">
+                    <p class="desc">
+                        Fred the baker baking company is out here making donuts dont you know?
+                        We make all the donuts. We make so many donuts you wouldnt even believe it.
+                        Thats all you need to know isnt it? We make donuts. You buy them. See you here.
+                    </p>
+                </div>
+            </div>
+        </li>
+    </ul>
+    <h2>Use our contact form and we will get back with you as soon as possible!</h2>
     <p>Phone: 555-555-5555</p>
     <p>Email: customerservice@accessaccessability.com</p>
-    <p>FAQ DropDown</p>
     <p>How can my business be added to your website?</p>
-    <p>How can I alert the community to my own experience at a location?</p>
-    <p>How do I leave a review?</p>
-    <p>What can I do if there is not a lot of content for my area?</p>
     </div>
     `;
 }
