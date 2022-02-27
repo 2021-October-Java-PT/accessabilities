0e6460280d94d4c4b4e0eba42516d441dfac2e41
CamS-World <cstephen13@mail.bw.edu>
2022-02-27 15:24:17 -0500

    Fixed app so it works, responsiveness still iffy

--- client-side/js/Components/Card.js
+++ client-side/js/Components/Card.js
@@ -1,11 +1,16 @@
 export default function Card(card) {
   return `
-
         <div class="review-card">
           <div class="location-wrapper">
           <div class="business-box business-name">${card.name}</div>
           <div class="business-box business-location"> ${card.businessCity}, ${card.businessState}</div>
-          <div class="business-box business-rating">Rating: C</div>
+          <div class="business-box business-rating">Rating: <div class="business-rating">
+          <i class="rating__star far fa-star"></i>
+          <i class="rating__star far fa-star"></i>
+          <i class="rating__star far fa-star"></i>
+          <i class="rating__star far fa-star"></i>
+          <i class="rating__star far fa-star"></i>
+        </div></div>
           <div class="business-box business-description">${card.businessDescription}</div>
           <div class="business-box business-reviews">
             <div class="comment-container">
@@ -20,23 +25,7 @@ export default function Card(card) {
                 <button type="submit" class="primaryContained float-right">Add Comment</button>
               </div>
           </div>
-
-          <div class="location-box a">${card.name}</div>
-          <div class="location-box b"> ${card.businessCity}, ${card.businessState}</div>
-          <div class="location-box c">Rating: C<div class="rating">
-          <i class="rating__star far fa-star"></i>
-          <i class="rating__star far fa-star"></i>
-          <i class="rating__star far fa-star"></i>
-          <i class="rating__star far fa-star"></i>
-          <i class="rating__star far fa-star"></i>
-        </div></div>
-          <div class="location-box d">${card.businessDescription}</div>
-          <div class="location-box e">Reviews</div>
-
           </div>
         </div>
-        
-        
-
     `;
 }
