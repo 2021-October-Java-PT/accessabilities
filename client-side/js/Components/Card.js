8d30acde044d47e00282ddb2c0fda7a3e319b02c
CamS-World <cstephen13@mail.bw.edu>
2022-02-27 16:28:23 -0500

    brought the cards back

--- client-side/js/Components/Card.js
+++ client-side/js/Components/Card.js
@@ -4,13 +4,13 @@ export default function Card(card) {
           <div class="location-wrapper">
           <div class="business-box business-name">${card.name}</div>
           <div class="business-box business-location"> ${card.businessCity}, ${card.businessState}</div>
-          <div class="business-box business-rating">Rating: <div class="business-rating">
+          <div class="business-box business-rating">
           <i class="rating__star far fa-star"></i>
           <i class="rating__star far fa-star"></i>
           <i class="rating__star far fa-star"></i>
           <i class="rating__star far fa-star"></i>
           <i class="rating__star far fa-star"></i>
-        </div></div>
+        </div>
           <div class="business-box business-description">${card.businessDescription}</div>
           <div class="business-box business-reviews">
             <div class="comment-container">
