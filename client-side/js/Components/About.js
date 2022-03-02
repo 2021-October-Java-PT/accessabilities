21bba0f4d062649755ce0d77eb4e67f474fc2b07
MartelBarkley <martel.barkley@gmail.com>
2022-03-02 11:55:08 -0800

    about page

--- client-side/js/Components/About.js
+++ client-side/js/Components/About.js
@@ -1,101 +1,112 @@
-
-
-export default function About(){
-    return`
-    <div class="about-body">
-        <div class="aboutContainer">
-            <div class="about-card">
-                <div class="about-content">
-                    <div class="imgBx"
-                        <img src="">
+export default function About() {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+        <head>
+           <meta charset="UTF-8">
+           <meta http-equiv="X-UA-Compatible" content="IE=edge">
+           <meta name="viewport" content="width=device-width, initial-scale=1.0">
+           <link rel="stylesheet" href="style.css">
+           <title>AccessABILITIES</title>
+        </head>
+        <body>
+            <div class="about-body">
+                <div class="aboutContainer">
+                    <div class="about-card">
+                        <div class="about-content">
+                            <div class="imgBx">
+                                <img src="https://media-exp1.licdn.com/dms/image/D4E35AQFIqfSUU3f08Q/profile-framedphoto-shrink_800_800/0/1646005587840?e=1646337600&v=beta&t=Knq-fncvmItgVJWZ60Y7PTIKNGCc2rKoA0uW8BErdHk"/>
+                            </div>
+                            <div class="about-content-box">
+                                <h1 class="about-h1">Elizabeth Hans<h1>
+                                <h2 class="about-h2">Software Developer<h2>
+                            </div>
+                            <div class="sci">
+                            <a onclick="parent.open('https://www.linkedin.com/in/lyzzhans/')"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                            <a onclick="parent.open('https://github.com/LyzzHans')"><i class="fa fa-github" aria-hidden="true"></i></a>
+                            </div>
+                        </div>
+                    </div> 
+                    <div class="about-card">
+                        <div class="about-content">
+                            <div class="imgBx">
+                                <img src="https://media-exp1.licdn.com/dms/image/C5603AQFDZulzi-nJoQ/profile-displayphoto-shrink_200_200/0/1565072659484?e=1651708800&v=beta&t=SaXhzc1aNXHrdo8-tYq4jp919ozccRPQaQo9XD7Yi_8"/>
+                            </div>
+                            <div class="about-content-box">
+                                <h1 class="about-h1">Dia Scruggs<h1>
+                                <h2 class="about-h2">Software Developer<h2>
+                            </div>
+                            <div class="sci">
+                            <a onclick="parent.open('https://www.linkedin.com/in/dia-scruggs-software-developer/')"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                            <a onclick="parent.open('https://github.com/DiaS793')"><i class="fa fa-github" aria-hidden="true"></i></a>
+                            </div>
+                        </div>
                     </div>
-                    <div class="about-content-box">
-                        <h1 class="about-h1">Lyzz Hans<h1>
-                        <h2 class="about-h2">Software Developer<h2>
+                    <div class="about-card">
+                        <div class="about-content">
+                            <div class="imgBx">
+                                <img src="https://media-exp1.licdn.com/dms/image/D4D03AQGC_Pupy5Outg/profile-displayphoto-shrink_800_800/0/1641313147062?e=1651708800&v=beta&t=AdWyPuTdZtrvN3lOvbErcuXy0fNg8jcOze_jtW-ajWM"/>
+                            </div>
+                            <div class="about-content-box">
+                                <h1 class="about-h1">Cameron Stephens<h1>
+                                <h2 class="about-h2">Software Developer<h2>
+                            </div>
+                            <div class="sci">
+                            <a onclick="parent.open('https://www.linkedin.com/in/cameron-stephens1/')"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                            <a onclick="parent.open('https://github.com/CamS-World')"><i class="fa fa-github" aria-hidden="true"></i></a>
+                            </div>
+                        </div>
                     </div>
-                    <div class="sci">
-                        <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
-                        <a href="#"><i class="fa fa-github" aria-hidden="true"></i></a>
+                    <div class="about-card">
+                        <div class="about-content">
+                            <div class="imgBx">
+                                <img src="https://media-exp1.licdn.com/dms/image/C4E03AQG60RB3a7-bTg/profile-displayphoto-shrink_800_800/0/1639167271823?e=1651708800&v=beta&t=I59CQS5ExeaW5Aos2cBbRQsuzt7gKY4-8J2BKPnKlmU"/>
+                            </div>
+                            <div class="about-content-box">
+                                <h1 class="about-h1">Trey Browder<h1>
+                                <h2 class="about-h2">Software Developer<h2>
+                            </div>
+                            <div class="sci">
+                            <a onclick="parent.open('https://www.linkedin.com/in/trey-browder7/')"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                            <a onclick="parent.open('https://github.com/TreyBrowder')"><i class="fa fa-github" aria-hidden="true"></i></a>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div> 
-            <div class="about-card">
-                <div class="about-content">
-                    <div class="imgBx"
-                        <img src="">
+                    <div class="about-card">
+                        <div class="about-content">
+                            <div class="imgBx">
+                                <img src="https://media-exp1.licdn.com/dms/image/C4D03AQGrgN_BYfgWvw/profile-displayphoto-shrink_800_800/0/1641316680136?e=1651708800&v=beta&t=bvOOAjYa2bHtO_SatKIB-MH7tG7mn9W0s-VJ1OX3XqY"/>
+                            </div>
+                            <div class="about-content-box">
+                                <h1 class="about-h1">Landon Sims<h1>
+                                <h2 class="about-h2">Software Developer<h2>
+                            </div>
+                            <div class="sci">
+                            <a onclick="parent.open('https://www.linkedin.com/in/landoncolesims/')"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                            <a onclick="parent.open('https://github.com/landoncolesims')"><i class="fa fa-github" aria-hidden="true"></i></a>
+                            </div>
+                        </div>
                     </div>
-                    <div class="about-content-box">
-                        <h1 class="about-h1">Dia Dcruggs<h1>
-                        <h2 class="about-h2">Software Developer<h2>
-                    </div>
-                    <div class="sci">
-                        <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
-                        <a href="#"><i class="fa fa-github" aria-hidden="true"></i></a>
+                    <div class="about-card">
+                        <div class="about-content">
+                            <div class="imgBx">
+                                <img src="https://i.ibb.co/vV5By6c/Martel.jpg"/>
+                            </div>
+                            <div class="about-content-box">
+                                <h1 class="about-h1">Martel Barkley<h1>
+                                <h2 class="about-h2">Software Developer<h2>
+                            </div>
+                            <div class="sci">
+                                <a onclick="parent.open('https://www.linkedin.com/in/martelbarkley/')"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                                <a onclick="parent.open('https://github.com/MartelBarkley')"><i class="fa fa-github" aria-hidden="true"></i></a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="about-card">
-                <div class="about-content">
-                    <div class="imgBx"
-                        <img src="">
-                    </div>
-                    <div class="about-content-box">
-                        <h1 class="about-h1">Cameron Stephens<h1>
-                        <h2 class="about-h2">Software Developer<h2>
-                    </div>
-                    <div class="sci">
-                        <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
-                        <a href="#"><i class="fa fa-github" aria-hidden="true"></i></a>
-                    </div>
-                </div>
-            </div>
-            <div class="about-card">
-                <div class="about-content">
-                    <div class="imgBx"
-                        <img src="">
-                    </div>
-                    <div class="about-content-box">
-                        <h1 class="about-h1">Trey Browder<h1>
-                        <h2 class="about-h2">Software Developer<h2>
-                    </div>
-                    <div class="sci">
-                        <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
-                        <a href="#"><i class="fa fa-github" aria-hidden="true"></i></a>
-                    </div>
-                </div>
-            </div>
-            <div class="about-card">
-                <div class="about-content">
-                    <div class="imgBx"
-                        <img src="">
-                    </div>
-                    <div class="about-content-box">
-                        <h1 class="about-h1">Landon Sims<h1>
-                        <h2 class="about-h2">Software Developer<h2>
-                    </div>
-                    <div class="sci">
-                        <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
-                        <a href="#"><i class="fa fa-github" aria-hidden="true"></i></a>
-                    </div>
-                </div>
-            </div>
-            <div class="about-card">
-                <div class="about-content">
-                    <div class="imgBx"
-                        <img src="./assets/images/Martel.jpg">
-                    </div>
-                    <div class="about-content-box">
-                        <h1 class="about-h1">Martel Barkley<h1>
-                        <h2 class="about-h2">Software Developer<h2>
-                    </div>
-                    <div class="sci">
-                        <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
-                        <a href="#"><i class="fa fa-github" aria-hidden="true"></i></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    
+        </body> 
+    </html>
       `;
 }
+
+
