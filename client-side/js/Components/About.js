f59c69f521236f1794594be51048ecf90800e708
MartelBarkley <martel.barkley@gmail.com>
2022-03-02 13:00:46 -0800

    fixes about page

--- client-side/js/Components/About.js
+++ client-side/js/Components/About.js
@@ -1,62 +1,3 @@
-
-
-
-export default function About(){
-    return`
-    <div class="page__container">
-    <div class="aboutDisplay">
-    <div class = "bio">
-        <h2>Lyzz Hans</h2>
-        
-        <p>Self motivated Software Developer  with a passion for improving the lives of individuals that experience barriers to upward mobility. Skilled in management, team building and working well under pressure. Diplomatic and highly organized when serving clients. Knowledge in agile development principles, Java, JavaScript, Spring, MVC,  HTML, CSS,  GitBash, TDD, and OOP.</p>
-        <h4>Contact</h4>
-        <a href="https://lyzzhans.com/"><li>Personal Site</li></a>
-        <a href = "mailto: lyzz.hans@gmail.com">Email</a>
-    </div>
-    <div class = "bio">
-        <h2>Landon Sims</h2>
-    
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <h4>Contact</h4>
-        <li>Personal Site: </li>
-        <li>Email: </li>
-    </div>
-    <div class = "bio">
-        <h2>Cameron Stephens</h2>
-        
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi libero nibh, cursus at eleifend a, sodales sit amet magna. Duis convallis orci venenatis nisl iaculis elementum. Nunc tempor nibh elit, id volutpat mi tempus tempor.</p>
-        <h4>Contact</h4>
-        <li>Personal Site: </li>
-        <li>Email: </li>
-    </div>
-    <div class = "bio">
-        <h2>Martel Barkley</h2>
-        
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <h4>Contact</h4>
-        <li>Personal Site: </li>
-        <li>Email: </li>
-    </div>
-    <div class = "bio">
-        <h2>Trey Browder</h2>
-       
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <h4>Contact</h4>
-        <li>Personal Site: </li>
-        <li>Email: </li>
-    </div>
-    <div class = "bio">
-    <h2>Dia Scruggs</h2>
-   
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    <h4>Contact</h4>
-    <li>Personal Site: </li>
-    <li>Email: </li>
-</div>
-  
-</div>
-    </div>
-
 export default function About() {
     return `
     <!DOCTYPE html>
@@ -166,9 +107,7 @@ export default function About() {
 
         </div>
     </div>
-    `;
-
-        </body> 
+     </body> 
     </html>
       `;
 
