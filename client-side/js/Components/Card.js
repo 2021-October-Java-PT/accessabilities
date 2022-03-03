a66e508252889bfb96e368d1006b88a825a0c0f1
CamS-World <cstephen13@mail.bw.edu>
2022-03-03 01:51:21 -0500

    Added cards comments and did some additional stylings

--- client-side/js/Components/Card.js
+++ client-side/js/Components/Card.js
@@ -2,9 +2,11 @@ export default function Card(card) {
   return `
         <div class="review-card">
           <div class="location-wrapper">
+          
           <div class="business-box business-name">${card.name}</div>
           <div class="business-box business-location"> ${card.businessCity}, ${card.businessState}</div>
           <div class="business-box business-rating">
+          <p>Leave a rating:</p>
           <i class="rating__star far fa-star"></i>
           <i class="rating__star far fa-star"></i>
           <i class="rating__star far fa-star"></i>
@@ -19,14 +21,19 @@ export default function Card(card) {
                 <div class="comment-body"></div>
               </div>
             </div>
-            <div class="comment-row">
-              <div class="col-6">
-                <textarea type="text" placeholder="Write a comment" class="input"></textarea>
+            <div class="comment-row-2"> 
+              <div class="col-6"> 
+                <input type="text" id="review-title" placeholder="Name"> <br></br>
+                <textarea type="text" placeholder="Write a comment" class="input" style="width: 600px; height: 140px;"></textarea>
                 <button type="submit" class="primaryContained float-right">Add Comment</button>
               </div>
           </div>
           </div>
         </div>
+        <div class="business-box comments-box">
+          <p class="try" ></p>
+        </div>
+
         <input type="hidden" id="businessId">
     `;
 }
